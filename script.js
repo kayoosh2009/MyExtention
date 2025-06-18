1ef791e416e79b936c58a0947d9d3deff39e2c32
Kai Miyamoto <kayoosh2009@gmail.com>
2025-06-18 18:13:56 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const iframe = document.getElementById('browser');
-const loader = document.getElementById('loader');
-
-// Показать iframe только после загрузки
-iframe.onload = () => {
-  loader.style.display = 'none';
-  iframe.style.display = 'block';
-};
